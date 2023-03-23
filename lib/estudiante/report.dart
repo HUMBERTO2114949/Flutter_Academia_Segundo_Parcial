473eb2adf9bc2e3348ab0c21040ea3208b9fe706
HUMBERTO2114949 <alu.2114949@itesrenedescartes.edu.mx>
2023-03-23 12:47:53 -0600

    Aplicacion movil con flutter para el segundo parcial

---
+++ lib/estudiante/report.dart
@@ -0,0 +1,145 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
+
+class reportt extends StatefulWidget {
+  DocumentSnapshot docid;
+  reportt({required this.docid});
+  @override
+  State<reportt> createState() => _reporttState(docid: docid);
+}
+
+class _reporttState extends State<reportt> {
+  DocumentSnapshot docid;
+  _reporttState({required this.docid});
+  final pdf = pw.Document();
+  var titulo;
+  var descripcion;
+  var link;
+
+  var marks;
+  void initState() {
+    setState(() {
+      titulo = widget.docid.get('Titulo');
+      descripcion = widget.docid.get('Descripcion');
+      link = widget.docid.get('link');
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PdfPreview(
+      // maxPageWidth: 1000,
+      // useActions: false,
+      // canChangePageFormat: true,
+      canChangeOrientation: false,
+      // pageFormats:pageformat,
+      canDebug: false,
+
+      build: (format) => generateDocument(
+        format,
+      ),
+    );
+  }
+
+  Future<Uint8List> generateDocument(PdfPageFormat format) async {
+    final doc = pw.Document(pageMode: PdfPageMode.outlines);
+
+    final font1 = await PdfGoogleFonts.openSansRegular();
+    final font2 = await PdfGoogleFonts.openSansBold();
+    // final image = await imageFromAssetBundle('assets/r2.svg');
+
+    String? _logo = await rootBundle.loadString('assets/r2.svg');
+
+    doc.addPage(
+      pw.Page(
+        pageTheme: pw.PageTheme(
+          pageFormat: format.copyWith(
+            marginBottom: 0,
+            marginLeft: 0,
+            marginRight: 0,
+            marginTop: 0,
+          ),
+          orientation: pw.PageOrientation.portrait,
+          theme: pw.ThemeData.withFont(
+            base: font1,
+            bold: font2,
+          ),
+        ),
+        build: (context) {
+          return pw.Center(
+              child: pw.Column(
+            mainAxisAlignment: pw.MainAxisAlignment.center,
+            children: [
+              pw.Flexible(
+                child: pw.SvgImage(
+                  svg: _logo,
+                  height: 100,
+                ),
+              ),
+              pw.SizedBox(
+                height: 20,
+              ),
+              pw.Center(
+                child: pw.Text(
+                  'Final Report card',
+                  style: pw.TextStyle(
+                    fontSize: 50,
+                  ),
+                ),
+              ),
+              pw.SizedBox(
+                height: 20,
+              ),
+              pw.Divider(),
+              pw.Row(
+                mainAxisAlignment: pw.MainAxisAlignment.center,
+                children: [
+                  pw.Text(
+                    'Titulo : ',
+                    style: pw.TextStyle(
+                      fontSize: 50,
+                    ),
+                  ),
+                  pw.Text(
+                    titulo,
+                    style: pw.TextStyle(
+                      fontSize: 50,
+                    ),
+                  ),
+                ],
+              ),
+              pw.Row(
+                mainAxisAlignment: pw.MainAxisAlignment.center,
+                children: [
+                  pw.Text(
+                    'Descripcion: ',
+                    style: pw.TextStyle(
+                      fontSize: 50,
+                    ),
+                  ),
+                  pw.Text(
+                    descripcion,
+                    style: pw.TextStyle(
+                      fontSize: 50,
+                    ),
+                  ),
+                ],
+              ),
+              pw.Divider(),
+            ],
+          ));
+        },
+      ),
+    );
+
+    return doc.save();
+  }
+}
---
+++ lib/programador/editpublic.dart
@@ -0,0 +1,160 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import 'programador.dart';
+import 'report.dart';
+
+class editnote extends StatefulWidget {
+  DocumentSnapshot docid;
+  editnote({required this.docid});
+
+  @override
+  _editnoteState createState() => _editnoteState(docid: docid);
+}
+
+class _editnoteState extends State<editnote> {
+  DocumentSnapshot docid;
+  _editnoteState({required this.docid});
+
+  TextEditingController titulo = TextEditingController();
+  TextEditingController descripcion = TextEditingController();
+  TextEditingController link = TextEditingController();
+
+  @override
+  void initState() {
+    titulo = TextEditingController(text: widget.docid.get('Titulo'));
+    descripcion = TextEditingController(text: widget.docid.get('Descripcion'));
+    link = TextEditingController(text: widget.docid.get('Link'));
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 0, 11, 133),
+        actions: [
+          MaterialButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (_) => Home()));
+            },
+            child: Text(
+              "Regresar",
+              style: TextStyle(
+                fontSize: 20,
+                color: Color.fromARGB(255, 251, 251, 251),
+              ),
+            ),
+          ),
+          MaterialButton(
+            onPressed: () {
+              widget.docid.reference.update({
+                'Titulo': titulo.text,
+                'Descripcion': descripcion.text,
+                'Link': link.text
+              }).whenComplete(() {
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (_) => Home()));
+              });
+            },
+            child: Text(
+              "Guardar",
+              style: TextStyle(
+                fontSize: 20,
+                color: Color.fromARGB(255, 251, 251, 251),
+              ),
+            ),
+          ),
+          MaterialButton(
+            onPressed: () {
+              widget.docid.reference.delete().whenComplete(() {
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (_) => Home()));
+              });
+            },
+            child: Text(
+              "Eliminar",
+              style: TextStyle(
+                fontSize: 20,
+                color: Color.fromARGB(255, 251, 251, 251),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                decoration: BoxDecoration(border: Border.all()),
+                child: TextField(
+                  controller: titulo,
+                  decoration: InputDecoration(
+                    hintText: 'Titulo',
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(border: Border.all()),
+                child: TextField(
+                  controller: descripcion,
+                  maxLines: null,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    hintText: 'Descripcion',
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(border: Border.all()),
+                child: TextField(
+                  controller: link,
+                  maxLines: null,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    hintText: 'link',
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              MaterialButton(
+                color: Color.fromARGB(255, 0, 11, 133),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => reportt(
+                        docid: docid,
+                      ),
+                    ),
+                  );
+                },
+                child: Text(
+                  "Generar Reporte",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Color.fromARGB(255, 251, 251, 251),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
